5eac60865c566a6b836488b73737e0422c1cca3f
nitin2991 <nitin.kamboj510@gmail.com>
2025-02-08 12:09:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    numberOfRegisterInOneTime: 6, // no of registration want to make in one command.
+    domains: ["lamoshitt.com", "crazzybabe.com"], // names of domain for the emails. 
+    oneTimeMailGen: 10 * 100000,
+    telegramBotToken: "7906075569:AAGVaGvKT0EL1MB5UAegXI9stGn5M_n0kXQ", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
+    adminNamesForTelegramBot: ["suraj_610","hahaha7726"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+    authorisedUserForTelegramBot: ["suraj_610","hahaha7726"] // username or id of the user who can use the bot.
+};
